86a58e552fd55c5913f79d282a5e07e39a1b646d
Marco de Haan <marco.dehaan@han.nl>
2019-05-17 12:32:17 +0200

    changed variable defaults

--- variables.tf
+++ variables.tf
@@ -1,47 +1,37 @@
 # set the variables to create VMs for bi2
 variable "prefix" {
-  default = "hannl-hlo-haanw"
+    default = ""
 }
-
-  variable "rgroup_name" {
-    default = "hannl-hlo-haanw-test007-rg"
-  }
-
-  variable "plan_name" {
-    default = "hannl-hlo-haanw-test007-rg-ddos-plan"
-  }
-
-  variable "nsg_name" {
-    default = "hannl-hlo-haanw-test007-rg-nsg"
-  }
-
-  variable "vnet_name" {
-    default = "hannl-hlo-haanw-test007-rg-vnet"
-  }
-    variable "vnet_addr" {
+variable "rgroup_name" {
+    default = "${var.prefix}-rg"
+    }
+variable "plan_name" {
+    default = "${var.rgroup_name}-ddos-plan"
+    }
+variable "nsg_name" {
+    default = "${var.rgroup_name}-nsg"
+    }
+variable "vnet_name" {
+    default = "${var.rgroup_name}-vnet"
+    }
+variable "vnet_addr" {
     default = "10.1.0.0/16"
-  }
-  
-  variable "subnet_name" {
-    default = "hannl-hlo-haanw-test007-rg-snet"
-  }
-
-  variable "subnet_addr" {
+    }
+variable "subnet_name" {
+    default = "${var.rgroup_name}-snet"
+    }
+variable "subnet_addr" {
     default = "10.1.0.0/24"
-  }
-  
-  variable "resource_location" {
+      }
+variable "resource_location" {
     default = "westeurope"
-  }
-
-  variable "vm_sku_size" {
+    }
+variable "vm_sku_size" {
     default = "Standard_B1S"
-  }
-
-  variable "cmd_user" {
+    }
+variable "cmd_user" {
     type = "string"
-  }
-
-  variable "cmd_pass" {
+    }
+variable "cmd_pass" {
     type = "string"
-  }
+    }
