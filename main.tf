384c997658320d45bec28d5a3eaf22deb334230d
Marco de Haan <marco.dehaan@han.nl>
2019-05-17 14:26:53 +0200

    changed subnet id reference

--- main.tf
+++ main.tf
@@ -106,7 +106,7 @@ resource "azurerm_network_interface" "main" {
 
   ip_configuration {
     name                          = "vm-ip-conf"
-    subnet_id                     = "${var.subnet_id}"
+    subnet_id                     = "${azurerm_subnet.main.id}"
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = "${azurerm_public_ip.main.id}"
   }
