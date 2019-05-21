c502ad0c1c6f73c597e929c6a73e167d757de485
Marco de Haan <marco.dehaan@han.nl>
2019-05-21 13:52:00 +0000

    foutje

--- variables.tf
+++ variables.tf
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-=======
-/*
-# set the variables to create VMs for bi2
-variable "rgroup_name" {
-    description = "Name of the resourcegroup"
-    default = "somedummyname-rg"
-    }
-variable "plan_name" {
-    description = "Name of the DDOS protectionplan"
-    default = "somedummyname-rg-ddos-plan"
-    }
-variable "nsg_name" {
-    description = "Name of the Network Security Group"
-    default = "somedummyname-rg-nsg"
-    }
-variable "vnet_name" {
-    description = "Name of the Virtual Network"
-    default = "somedummyname-rg-vnet"
-    }
-variable "vnet_addr" {
-    description = "VNET Address range e.g. 10.1.0.0/16"
-    default = "10.1.0.0/16"
-    }
-variable "subnet_name" {
-    description = "Name of the Subnet"
-    default = "somedummyname-rg-snet"
-    }
-variable "subnet_addr" {
-    description = "SubNet Address Range e.g. 10.1.0.0/24"
-    default = "10.1.0.0/24"
-    }
-variable "resource_location" {
-    description = "Location to put all resources"
-    default  = "westeurope"
-    }
-*/
-
->>>>>>> c86af040f18b188d3ad8d2f05f3a474e1502d320
 variable "vm_name" {
     description = "Name for the virtual machine"
     default = "somedummyname-vm"
@@ -63,7 +24,6 @@ variable "cmd_pass" {
     default = "s0M3Stup1dPassW0rd"
     type = "string"
     }
-
 variable "subnet_id" {
     description = "id of the subnet to use for the VM"
 }
